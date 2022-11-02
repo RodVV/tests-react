edf4b0999ce6c9af941cc4bad607e7a74e0caa7a
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-11-02 20:16:21 -0300

    ultimo commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event'
+
+describe('Teste App.js', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test('Verifica o funcionamento do link Home', async () => {
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+    const homeScreen = await 
+
+  });
+});
